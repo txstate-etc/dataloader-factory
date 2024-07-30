e9b8db835088cfc0c93ca5a632d4689a764b86de
Nick Wing <nwing@txstate.edu>
2024-07-30 10:25:44 -0500

    export the new ParentDocumentLoader in ESM, release 4.6.1

--- dist-esm/index.js
+++ dist-esm/index.js
@@ -6,6 +6,7 @@ export const ManyToManyLoader = dlf.ManyToManyLoader
 export const ManyJoinedLoader = dlf.ManyJoinedLoader
 export const OneToManyLoader = dlf.OneToManyLoader
 export const BestMatchLoader = dlf.BestMatchLoader
+export const ParentDocumentLoader = dlf.ParentDocumentLoader
 
 export const Loader = dlf.Loader
 export const BaseManyLoader = dlf.BaseManyLoader
